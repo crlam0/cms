b66ff4e345a31340d474c032394c01fa188314c8
Igor Krasnov <boot@ngs.ru>
2017-06-21 15:36:01 +0700

    Upgrade CKEditor to latest version

--- include/js/editor.js
+++ include/js/editor.js
@@ -1,13 +1,12 @@
 $(document).ready(function(){
 	CKEDITOR.replace( 'editor',
 	{
-		skin : 'office2003',
-                extraPlugins : 'tableresize',
                 language : 'ru',
                 width : 900,
                 height : 400,
 		filebrowserBrowseUrl : '../include/filemanager/index.html'
 	});
+
 });
 
 
--- include/js/editor_mini.js
+++ include/js/editor_mini.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
 	CKEDITOR.replace( 'editor',
 	{
-		skin : 'office2003',
-                extraPlugins : 'tableresize',
                 language : 'ru',
                 width : 900,
                 height : 200,

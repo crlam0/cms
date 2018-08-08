38e1e58ce453e53fc1e4f64d8a3bbab4b825ea19
Igor Krasnov <boot@ngs.ru>
2018-08-08 12:57:26 +0700

    Misc changes

--- include/js/misc.js
+++ include/js/misc.js
@@ -6,6 +6,22 @@ function test() {
 	}
 }
 
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+
 if(jQuery && !jQuery.fn.live) {
     jQuery.fn.live = function(evt, func) {
         $('body').on(evt, this.selector, func);

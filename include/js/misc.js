e1da2d6fb2510a55916b4ac594f57171e8095556
Igor Krasnov <boot@ngs.ru>
2018-11-09 08:34:52 +0700

    Fix bugs and warnings in gallery_edit.php

--- include/js/misc.js
+++ include/js/misc.js
@@ -27,3 +27,44 @@ if(jQuery && !jQuery.fn.live) {
         $('body').on(evt, this.selector, func);
     };
 }
+
+/*
+(function($) {
+
+    $.fn.print_r = $.fn.print = function(variable){
+        return this.each(function(){
+        if(typeof variable == 'object'){
+            var string = $.print_r.objectToString(variable,0);
+            $(this).html(string);
+        } else {
+            $(this).html('<pre>'+variable.toString()+'</pre>');
+        }
+    });
+
+    }
+
+    $.print_r = {
+            objectToString : function (variable,i){
+              var string = '';
+              if(typeof variable == 'object' && i < 3){ // 3 is to prevent endless recursion, set higher for more depth
+                  string += 'Object ( <ul style="list-style:none;">';
+                  var key;
+                  for(key in variable) {
+                      if (variable.hasOwnProperty(key)) {
+                        string += '<li>['+key+'] => ';
+                        string += $.print_r.objectToString(variable[key],i+1);
+                        string += '</li>';
+                      }
+                  }
+                  string += '</ul> )';
+              } else {
+                  string = variable.toString();
+              }
+              return string;
+        }
+    }
+
+})(jQuery)
+
+
+  */

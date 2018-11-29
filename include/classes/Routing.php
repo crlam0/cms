bb3f5545246ff714743cc1a5be1d2bb3867a74fc
Igor Krasnov <boot@ngs.ru>
2018-11-30 00:14:34 +0700

    Fix bug in Routing.php

--- include/classes/Routing.php
+++ include/classes/Routing.php
@@ -39,7 +39,7 @@ final class Routing {
     }
     
     public function hasGETParams () {
-        return strpos($this->request_uri,'?')>0;
+        return strpos($this->request_uri,'?')!==false;
     }
 
     public function proceedGETParams () {

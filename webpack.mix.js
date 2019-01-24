6899a7fd3e14310bd17548c9982fe278f5f0b534
Igor Krasnov <boot@ngs.ru>
2019-01-24 08:30:30 +0700

    Change in webpack.js

--- webpack.mix.js
+++ webpack.mix.js
@@ -6,10 +6,10 @@ mix
     .js('theme/assets/js/vendor.js', 'js')
     .js('theme/assets/js/main.js', 'js')
     .sass('theme/assets/sass/full.scss', 'css')
-/*  
     .copyDirectory('theme/assets/images', 'theme/images')
+/*  
     .copyDirectory('theme/assets/fonts', 'theme/fonts') 
- */
+*/
     .options({
        processCssUrls: true
     })

46594cbfb55495a18cca1d46a4fbb03d90708f1f
Igor Krasnov <boot@ngs.ru>
2019-11-24 15:23:36 +0600

    Add console application with cache clear and Phinx commands.

---
+++ bin/app.php
@@ -0,0 +1,33 @@
+#!/usr/bin/env php
+
+<?php
+require __DIR__.'/../include/config/config.local.php';
+require __DIR__.'/../include/config/misc.php';
+$DIR = __DIR__.'/../';
+
+if(file_exists($DIR.'vendor/autoload.php')) {
+    require_once $DIR.'vendor/autoload.php';
+} else {
+    die('Cant find autoloader');
+}
+
+use Classes\App;
+use Symfony\Component\Console\Application;
+use Phinx\Console\Command;
+
+$App = new App($DIR, $SUBDIR);
+$App->connectDB($DBHOST, $DBUSER, $DBPASSWD, $DBNAME);
+unset($DBHOST, $DBUSER, $DBPASSWD, $DBNAME);
+
+require_once $DIR__.'include/lib_functions.php';
+
+$cli = new Application('Application console');
+$cli->add(new Classes\Command\CacheClearCommand($console['cachePaths']));
+$cli->addCommands([
+    new Command\Create(),
+    new Command\Migrate(),
+    new Command\Rollback(),
+    new Command\Status(),
+]);
+$cli->run();
+
--- include/config/misc.php
+++ include/config/misc.php
@@ -1,22 +1,28 @@
 <?php
 
-// $JQUERY_INC='<script type="text/javascript" src="'.$BASE_HREF.'include/js/jquery.min.js"></script>'."\n";
-$JQUERY_INC=''."\n";
-$JQUERY_FORM_INC='<script type="text/javascript" src="'.$BASE_HREF.'include/js/jquery.form.js"></script>'."\n";
+$JQUERY_INC='';
+$JQUERY_FORM_INC='<script type="text/javascript" src="'.$SUBDIR.'include/js/jquery.form.js"></script>'."\n";
 
-$EDITOR_INC='<script type="text/javascript" src="'.$BASE_HREF.'include/ckeditor/ckeditor.js" charset="utf-8"></script>'."\n".
-'<script type="text/javascript" src="'.$BASE_HREF.'include/js/editor.js"></script>'."\n";
+$EDITOR_INC='<script type="text/javascript" src="'.$SUBDIR.'include/ckeditor/ckeditor.js" charset="utf-8"></script>'."\n".
+'<script type="text/javascript" src="'.$SUBDIR.'include/js/editor.js"></script>'."\n";
 
-$EDITOR_MINI_INC= ' <script type="text/javascript" src="'.$BASE_HREF.'include/ckeditor/ckeditor.js" charset="utf-8"></script>'."\n".
-'<script type="text/javascript" src="'.$BASE_HREF.'include/js/editor_mini.js"></script>'."\n";
+$EDITOR_MINI_INC= ' <script type="text/javascript" src="'.$SUBDIR.'include/ckeditor/ckeditor.js" charset="utf-8"></script>'."\n".
+'<script type="text/javascript" src="'.$SUBDIR.'include/js/editor_mini.js"></script>'."\n";
 
-$EDITOR_HTML_INC='<script type="text/javascript" src="'.$BASE_HREF.'include/edit_area/edit_area_full.js" charset="utf-8">></script>'."\n".
-'<script type="text/javascript" src="'.$BASE_HREF.'include/js/editor_html.js" charset="utf-8"></script>'."\n";
+$EDITOR_HTML_INC='<script type="text/javascript" src="'.$SUBDIR.'include/edit_area/edit_area_full.js" charset="utf-8">></script>'."\n".
+'<script type="text/javascript" src="'.$SUBDIR.'include/js/editor_html.js" charset="utf-8"></script>'."\n";
 
-$tags['nav_str']="<a href={$SUBDIR} class=nav_home>Главная</a>";
+$tags['nav_str']='<a href='.$SUBDIR.' class="nav_home">Главная</a>';
 $tags['nav_array'][] = [
     'url' => '',
     'title' => 'Главная'
 ];
 
-
+$console = [
+    'cachePaths' => [
+        'twig' => 'var/cache/twig',
+        'catalog' => 'var/cache/catalog',
+        'gallery' => 'var/cache/gallery',
+    ],
+];
+    

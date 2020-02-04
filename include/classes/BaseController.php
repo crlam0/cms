0ea68b9ff8cce03d632717d80171e50c06602c6e
Igor Krasnov <boot@ngs.ru>
2020-02-04 13:34:13 +0700

    Add comment to include/classes/BaseController.php

--- include/classes/BaseController.php
+++ include/classes/BaseController.php
@@ -17,6 +17,14 @@ class BaseController {
     */
     public $tags = [];
     
+    /**
+     * Check method and run it if exists
+     *
+     * @param string $methodName Method name
+     * @param array $params Params array
+     *
+     * @return string Content
+     */
     private function runMethod(string $methodName, array $params = []) {
         if (method_exists($this, $methodName)) {
             $method = new \ReflectionMethod($this, $methodName);
@@ -27,6 +35,14 @@ class BaseController {
         throw new \InvalidArgumentException('Method ' . $methodName . ' not found.');
     }
     
+    /**
+     * Run action with params
+     *
+     * @param string $action Action name
+     * @param array $params Params array
+     *
+     * @return string Content
+     */
     public function run(string $action, array $params = []) {
         $method = 'action' . str_replace(' ', '', ucwords(implode(' ', explode('-', $action))));
         return $this->runMethod($method, $params);
